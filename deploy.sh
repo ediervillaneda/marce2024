58f683fb29972c0d2d564d0bb89c46cedaa440d3
Edier Andres Villaneda Navarro <ediervillaneda@gmail.com>
2024-08-12 13:55:57 -0500

    Deploy to Github Pages

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/ediervillaneda/marce2024.git main:gh-pages
+ git push -f git@github.com:ediervillaneda/marce2024.git master:gh-pages
+# git push -f https://github.com/ediervillaneda/marce2024.git main:gh-pages
 
 cd -
